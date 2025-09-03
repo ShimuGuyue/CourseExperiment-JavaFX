cc72c713af6da846bec95864b12bf269fb3a2cb6
ShimuGuyue <2147539031@qq.com>
2025-09-03 19:05:39 +0800

    Update JavaFX1\src\Main.java
    编写一个简单的图形化界面的Java应用程序

--- JavaFX1/src/Main.java
+++ JavaFX1/src/Main.java
@@ -1,4 +1,26 @@
-public class Main {
+import javafx.application.Application;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
     public static void main(String[] args) {
+        launch(args);
+    }
+
+    /**
+     *  编写一个简单的图形化界面的Java应用程序
+     */
+    public void start(Stage stage) {
+        stage.setTitle("JavaFX程序");
+
+        Label label = new Label("这是我的第一个JavaFX程序");
+        StackPane root = new StackPane();   // StackPane 默认居中对齐
+        root.getChildren().add(label);
+        Scene scene = new Scene(root, 415, 411);
+        stage.setScene(scene);
+
+        stage.show();
     }
 }
